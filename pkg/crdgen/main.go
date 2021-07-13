4ce17f32471574757bfca591e4a1c3b1f55021fa
Nick Gerace <nick.gerace@suse.com>
2021-07-13 18:04:49 -0400

    Create both v1 and v1beta1 apiext crds

--- pkg/crdgen/main.go
+++ pkg/crdgen/main.go
@@ -2,43 +2,58 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
-	v1 "github.com/rancher/gitjob/pkg/apis/gitjob.cattle.io/v1"
+	gitjobv1 "github.com/rancher/gitjob/pkg/apis/gitjob.cattle.io/v1"
 	"github.com/rancher/wrangler/pkg/crd"
 	_ "github.com/rancher/wrangler/pkg/generated/controllers/apiextensions.k8s.io"
 	"github.com/rancher/wrangler/pkg/schemas/openapi"
 	"github.com/rancher/wrangler/pkg/yaml"
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
+var NamespacedType = "GitJob.gitjob.cattle.io/v1"
+
 func main() {
-	var crds []crd.CRD
-	crds = append(crds,
-		crd.NamespacedType("GitJob.gitjob.cattle.io/v1").
-			WithStatus().
-			WithSchema(mustSchema(v1.GitJob{})).
-			WithColumnsFromStruct(v1.GitJob{}).
-			WithCustomColumn(apiextv1.CustomResourceColumnDefinition{
-				Name:     "Age",
-				Type:     "date",
-				JSONPath: ".metadata.creationTimestamp",
-			}))
+	fmt.Println("{{- if .Capabilities.APIVersions.Has \"apiextensions.k8s.io/v1\" -}}")
+	fmt.Println(generateGitJobCrd())
+	fmt.Println("{{- else -}}")
+	fmt.Println(generateGitJobCrdV1Beta1())
+	fmt.Println("{{- end -}}")
+}
 
-	var result []runtime.Object
-	for _, crd := range crds {
-		crdObject, err := crd.ToCustomResourceDefinition()
-		if err != nil {
-			panic(err)
-		}
-		result = append(result, crdObject)
-	}
-
-	output, err := yaml.Export(result...)
+func generateGitJobCrd() string {
+	crdObject, err := crd.NamespacedType(NamespacedType).
+		WithStatus().
+		WithSchema(mustSchema(gitjobv1.GitJob{})).
+		WithColumnsFromStruct(gitjobv1.GitJob{}).
+		WithCustomColumn(apiextv1.CustomResourceColumnDefinition{
+			Name:     "Age",
+			Type:     "date",
+			JSONPath: ".metadata.creationTimestamp",
+		}).ToCustomResourceDefinition()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(output))
+	return generateYamlString(crdObject)
+}
+
+func generateGitJobCrdV1Beta1() string {
+	crdObject, err := crd.NamespacedType(NamespacedType).
+		WithStatus().
+		WithSchemaV1Beta1(mustSchemaV1Beta1(gitjobv1.GitJob{})).
+		WithColumnsFromStructV1Beta1(gitjobv1.GitJob{}).
+		WithCustomColumnV1Beta1(apiextv1beta1.CustomResourceColumnDefinition{
+			Name:     "Age",
+			Type:     "date",
+			JSONPath: ".metadata.creationTimestamp",
+		}).ToCustomResourceDefinitionV1Beta1()
+	if err != nil {
+		panic(err)
+	}
+	return generateYamlString(crdObject)
 }
 
 func mustSchema(obj interface{}) *apiextv1.JSONSchemaProps {
@@ -47,4 +62,20 @@ func mustSchema(obj interface{}) *apiextv1.JSONSchemaProps {
 		panic(err)
 	}
 	return result
+}
+
+func mustSchemaV1Beta1(obj interface{}) *apiextv1beta1.JSONSchemaProps {
+	result, err := openapi.ToOpenAPIFromStructV1Beta1(obj)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func generateYamlString(crdObject runtime.Object) string {
+	crdYaml, err := yaml.Export(crdObject)
+	if err != nil {
+		panic(err)
+	}
+	return strings.Trim(string(crdYaml), "\n")
 }
