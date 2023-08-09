54437f240688625d2d3201795e5640316d08ddfc
Mario Manno <mario.manno@suse.com>
2023-08-09 14:48:23 +0200

    Remove the job cache from apply, so jobs can be recreated

--- pkg/controller/gitjob/gitjobs.go
+++ pkg/controller/gitjob/gitjobs.go
@@ -49,7 +49,9 @@ func Register(ctx context.Context, cont *types.Context) {
 	v1controller.RegisterGitJobGeneratingHandler(
 		ctx,
 		cont.Gitjob.Gitjob().V1().GitJob(),
-		cont.Apply.WithSetOwnerReference(true, false).WithCacheTypes(cont.Batch.Batch().V1().Job(), cont.Core.Core().V1().Secret()).WithPatcher(
+		cont.Apply.WithSetOwnerReference(true, false).
+			WithDynamicLookup().
+			WithCacheTypes(cont.Core.Core().V1().Secret()).WithPatcher(
 			batchv1.SchemeGroupVersion.WithKind("Job"),
 			func(namespace, name string, patchType types2.PatchType, data []byte) (runtime.Object, error) {
 				return nil, apply.ErrReplace
