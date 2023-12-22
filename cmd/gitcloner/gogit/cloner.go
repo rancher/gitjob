307a0885a0e5691c2b9b9f929c479254604f8e4b
raul <raul.cabello@suse.com>
2023-12-22 16:17:36 +0100

    fix git clone in azure devops

--- cmd/gitcloner/gogit/cloner.go
+++ cmd/gitcloner/gogit/cloner.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/protocol/packp/capability"
 	"github.com/go-git/go-git/v5/plumbing/transport"
 	httpgit "github.com/go-git/go-git/v5/plumbing/transport/http"
 	gossh "github.com/go-git/go-git/v5/plumbing/transport/ssh"
@@ -37,6 +38,12 @@ func NewCloner() *Cloner {
 }
 
 func (c *Cloner) CloneRepo(opts *cmd.Options) error {
+	// Azure DevOps requires capabilities multi_ack / multi_ack_detailed,
+	// which are not fully implemented and by default are included in
+	// transport.UnsupportedCapabilities.
+	transport.UnsupportedCapabilities = []capability.Capability{
+		capability.ThinPack,
+	}
 	auth, err := createAuthFromOpts(opts)
 	if err != nil {
 		return err
